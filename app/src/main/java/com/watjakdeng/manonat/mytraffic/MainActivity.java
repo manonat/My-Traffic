0b03514c5bcfb31b9680dea470b667b9b1ccd604
manonat <buddhacomic@hotmail.com>
2016-06-04 14:07:46 +0700

    วิธีดูclass methoe หstatement

--- app/src/main/java/com/watjakdeng/manonat/mytraffic/MainActivity.java
+++ app/src/main/java/com/watjakdeng/manonat/mytraffic/MainActivity.java
@@ -7,7 +7,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); // นี่คือ คำสั่ง หรือ Statement
         setContentView(R.layout.activity_main);
-    }
-}
+    }  // Main Method นี่คือ เมธอดหลัก
+
+} //Main class นี่คือ คลาสหลักจร้า
